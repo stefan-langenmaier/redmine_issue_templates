d731181d7f2a3a7b9b76730b176f53ca50389e0c
Akiko Takano <akiko.pusu@gmail.com>
2012-12-08 21:25:33 +0900

    Merge

---
+++ app/models/issue_template.rb
@@ -0,0 +1,23 @@
+class IssueTemplate < ActiveRecord::Base
+  include Redmine::SafeAttributes
+  unloadable
+  belongs_to :project
+  belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
+  belongs_to :tracker
+  validates :project_id, :presence => true
+  validates :title, :presence => true
+  validates :tracker, :presence => true
+  validates_uniqueness_of :title, :scope => :project_id
+  acts_as_list :scope => :tracker
+  
+  # author and project should be stable.
+  safe_attributes 'title', 'description', 'tracker_id', 'note', 'enabled', 'issue_title'
+                   
+  def enabled?
+    self.enabled == true
+  end
+  
+  def <=>(issue_template)
+    position <=> issue_template.position
+  end  
+end
